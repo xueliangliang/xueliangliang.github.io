2b76970e1fc441d872650e68f03601dae82b253e
Xue Liangliang <liangliang@popularworld.com>
2016-10-14 10:02:40 +0800

    touchup on ui

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,38 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var compass = require('compass-importer');
+var swig = require('gulp-swig');
+var data = require('gulp-data');
+var path = require('path');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var getJsonData = function(file) {
+   return require('./data/' + path.basename(file.path).slice(0, -5) + '.json'); 
+}
+
+gulp.task('default', ['sass','templates'],function() {
+    console.log("Completed!");
 });
 
 gulp.task('sass',function() {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass({
-            includePaths: require('node-reset-scss').includePath
+            includePaths: require('node-reset-scss').includePath,
+            importer: compass
         }).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
+gulp.task('templates', function() {
+  gulp.src('./templates/*.html')
+    .pipe(data(getJsonData))
+    .pipe(swig())
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('sass:watch', function () {
-      gulp.watch('./scss/**/*.scss', ['style']);
+      gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('templates:watch', function () {
+      gulp.watch(['./templates/*.html','./data/*.json'], ['templates']);
 });
